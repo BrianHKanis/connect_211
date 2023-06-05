3a6411f9695bb20d1fe45af1939d1d2ec88b22d3
BrianHKanis <brianhkanis@gmail.com>
2023-06-05 14:12:14 -0600

    Added files

--- core-tables-simple.py
+++ core-tables-simple.py
@@ -5,8 +5,7 @@ from build_tables.core_tables import organizations, services, locations, service
 from build_tables import tables
 import json
 
-all_tables = tables.build_all()
+organizations_table = tables.build_dict('organizations')
 
-for k, v in all_tables.items():
-    with open(f'new_data/{k}.json', 'w') as somejson:
-        json.dump(v, somejson, indent=4, sort_keys=True)
+services_table = tables.build_dict('services')
+locations_table = tables.build_dict('locations')
